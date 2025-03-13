638ecae1711a5a2c6e912ddd227ffcc3ce701aae
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-03-13 19:15:07 -0300

    Adding date formarter to the prompt view

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import * as p from '@clack/prompts';
 import { type } from 'os';
 import color from 'picocolors'
 import {setTimeout} from 'timers/promises';
+import { formatDistanceToNow } from 'date-fns';
+import { title } from 'process';
 
 const types = ['PushEvent', 'CreateEvent', 'WatchEvent']
 
@@ -22,7 +24,7 @@ function analizeActivities(actions){
   console.log(actions)
   for(let action of actions){
     if(action.type === 'PushEvent'){
-      console.log(color.green(`Pushed ${action.payload.commits.length} commits to ${action.payload.ref} at ${action.repo.name}`), color.white(`- ${action.created_at}`))
+      console.log(color.green(`Pushed ${action.payload.commits.length} commits to ${action.payload.ref} at ${action.repo.name}`), color.white(`- ${formatDistanceToNow(action.created_at, {addSuffix: true})}`))
     }
     if(action.type === 'CreateEvent'){
       console.log(color.green(`Created ${action.payload.ref_type} ${action.payload.ref} at ${action.repo.name}`))
@@ -36,10 +38,11 @@ function analizeActivities(actions){
 
 async function main() {
   p.intro('Welcome to Github Activity CLI', 'A CLI tool to get the latest activities of a Github user')
-  const username = await p.text({message: 'Enter the Github username', placeholder: 'octocat'});
+  const username = await p.text({message: 'Enter the Github username', placeholder: 'guilhermezuriel', defaultValue: 'guilhermezuriel'});
   console.log(color.bold(`Fetching the latest activities of ${username}...`))
   const data = await fetchActivities(username);
   analizeActivities(data);
+  p.outro('Thanks for using Github Activity CLI', 'Hope you liked it!')
 }
 
 main();
