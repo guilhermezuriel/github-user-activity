704ad7452a740e175ed7ce3f573ca528daaa4bd1
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-03-19 23:57:24 -0300

    cool improvment ideas to up the cli

--- index.js
+++ index.js
@@ -45,4 +45,26 @@ async function main() {
   // p.outro('Thanks for using Github Activity CLI', 'Hope you liked it!')
 }
 
+/*
+IMPROVMENT IDEAS:
+- Add more types of activities
+- Add more options to the user
+- Add more information to the user
+- Add more colors to the output
+- Add more error handling
+- Add more tests
+- Add more documentation
+
+
+- Add visual template of github's activity board
+- Should be able to choose between public activities, log in to see private repositories
+- Using ssh authentication to fetch data and provide more information
+- Logging via terminal to get token and provide more information
+- Stabelish goals of activities, stored in a repository on github and fetch data from it
+- Review the goals to see if they are being met
+- List commits made by that user of a specific repository, branch, etc 
+- Organization mode, to quickly see the activities of the members involved
+
+*/
+
 main();
